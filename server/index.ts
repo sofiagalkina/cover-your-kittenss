aaf32b3315ab6224dc852e4ef2ff95be6a9ff2ab
sofiagalkina <gal18024@byui.edu>
2025-06-25 13:31:06 -0600

    fixing port

--- server/index.ts
+++ server/index.ts
@@ -48,6 +48,7 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3001, () => {
-  console.log('✅ Socket.io server running on port 3001');
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+    console.log(`✅ Socket.io server running on port ${PORT}`)
 });
