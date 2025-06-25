0d1e6eb194a922746ec8fe235d8f00c7afda8243
sofiagalkina <gal18024@byui.edu>
2025-06-25 13:12:10 -0600

    CORS reconfig to new URL

--- server/index.ts
+++ server/index.ts
@@ -7,7 +7,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://cover-your-kittenss.vercel.app',
     methods: ['GET', 'POST'],
     credentials: true,
   },
