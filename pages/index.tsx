d6ea60db87fed70152ea0358131004f57636a5ec
sofiagalkina <gal18024@byui.edu>
2025-07-18 22:28:38 -0400

    tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,11 +12,11 @@ export default function Home() {
   };
 
   return (
-    <div className="p-8 text-center">
-      <h1 className="text-4xl font-bold mb-6">Welcome to the Game</h1>
+    <div className="p-8 text-center text-white ">
+      <h1 className="text-4xl font-bold mb-6 ">Welcome to Cover Your Kittens!</h1>
       <input
         type="text"
-        placeholder="Enter Room Code"
+        placeholder="Enter Room ID"
         value={code}
         onChange={(e) => setCode(e.target.value)}
         className="border p-2 mr-2"
